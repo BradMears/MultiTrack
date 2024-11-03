bfd1a965ab05ad6edab7dd6cf097dcbc95b6510d
Brad Mears <brad.mears@gmail.com>
2024-11-03 03:20:29 +0000

    Minor refactoring on a unit test

--- SatTrack1/tests/test_satelite_pass.py
+++ SatTrack1/tests/test_satelite_pass.py
@@ -1,8 +1,5 @@
 '''pytest for the SatellitePass class.'''
 
-# This is a pretty sparse test suite. It could use some more test cases to
-# exercise SatellitePass directly and not just upcoming_passes(). 
-
 import pytest
 import pytz
 import json
@@ -15,29 +12,31 @@ from skyfield.api import EarthSatellite
 # Uses a JSON file from a saved date so the results won't change as new orbital
 # elements are released.
 
-# Set the observer coordinates and timezone 
-lat = 38.9596
-lon = -104.7695
-elev = 2092
-obs_pos = wgs84.latlon(latitude_degrees=lat, longitude_degrees=lon, elevation_m = elev)
+def set_globals():
+    '''This is almost certainly an anti-pattern but I haven't seen a better way
+    to do one-time initialization of a handful of objects in pytest. I'm sure 
+    I'll find a better way and look at this aghast that I did it this way.'''
 
-TZ_STRING = "America/Denver"
-TZ = pytz.timezone(TZ_STRING)
-SatellitePass.TZ = TZ
+    # Set the observer coordinates and timezone 
+    lat = 38.9596
+    lon = -104.7695
+    elev = 2092
 
-# Get the fake time in skyfield format and in regular python datetime
-ts = load.timescale()
-t = Time(tt=2460617.3960255613, ts=ts)  # Time is stuck at 3:29 PM on Nov 2, 2024
-dt = t.utc_datetime()
-t_end = t + 4 / 24
+    SatellitePass.TZ = pytz.timezone("America/Denver")
+    
+    # Get the fake time in skyfield format and in regular python datetime
+    ts = load.timescale()
 
-with load.open('amateur-241102.json') as f:
-    amsats_json = json.load(f)
-amsats = [EarthSatellite.from_omm(ts, fields) for fields in amsats_json]
+    with load.open('amateur-241102.json') as f:
+        amsats_json = json.load(f)
 
-def get_sat_by_num(satnum):
-    sat = next((x for x in amsats if x.model.satnum == satnum), None)
-    return sat
+    # Anything the test cases want to use are stores in pytest
+    pytest.amsats = [EarthSatellite.from_omm(ts, fields) for fields in amsats_json]
+    pytest.t = Time(tt=2460617.3960255613, ts=ts)  # Time is stuck at 3:29 PM on Nov 2, 2024
+    pytest.t_end = pytest.t + 4 / 24
+    pytest.obs_pos = wgs84.latlon(latitude_degrees=lat, longitude_degrees=lon, elevation_m = elev)
+
+set_globals()
 
 @pytest.mark.parametrize("satnum, passes, fails", 
                          [(14781, 1, 0), 
@@ -46,9 +45,12 @@ def get_sat_by_num(satnum):
                           (53106, 0, 1),
                           (44854, 0, 0) ])
 def test_passes_and_fails(satnum, passes, fails):
-    sat = get_sat_by_num(satnum)
+    sat = next((x for x in pytest.amsats if x.model.satnum == satnum), None)
     # Find all upcoming passes over 30 degrees in the desired timeframe
-    sat_passes, failed_passes = upcoming_passes(obs_pos, sat, 30.0, t, t_end)
+    sat_passes, failed_passes = upcoming_passes(pytest.obs_pos, sat, 30.0, pytest.t, pytest.t_end)
     assert len(sat_passes) == passes
     assert len(failed_passes) == fails
+
+# This is a pretty sparse test suite. It could use some more test cases to
+# exercise SatellitePass directly and not just upcoming_passes(). 
 
