ea3533e07062ab02963335386c3485aae4c9524c
Brad Mears <brad.mears@gmail.com>
2025-10-01 03:23:53 +0000

    Got SatTrack3 working to the point of needing to connect to the G5500

--- SatTrack3/pass_predictor.py
+++ SatTrack3/pass_predictor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-'''Prints a list of upcoming passes for various satellites. Arguments can be 
+'''UI to control the G5500 by sending position commands to a separate g5500_srvc process.
+Allos the user to select an upcoming satellite pass from a list. Arguments can be 
 specified on the command-line or in the config-file observer.txt' The command-line
 takes precedence over the config-file.'''
 
@@ -32,34 +33,9 @@ parser.add_argument('--max_range', type=int, default=6000, help='Upper range lim
 parser.add_argument('--max_hours', type=float, default=4, help = 'Maximum look-ahead time')
 parser.add_argument('--sat_name', type=str, default="", help = 'Only show satellites whose name starts with this string')
 parser.add_argument('--cat_number', type=int, default="-1", help = 'Only show the satellite with this catalog ID')
-parser.add_argument('--tle_file', type=str, default="", help = 'Name of TLE file to use')
-
-"""
-def load_from_file_or_url(group_name, max_days=7.0):
-    '''Loads Satellite data. First looks for a local file. If that doesn't 
-    exist or is older than max_days, goes to the Celestrak site.'''
-    filename = f'{group_name}.json'  # custom filename, not 'gp.php'
-
-    # You can use https on the following request but my work laptop complains about a self-signed certificate
-    # when I do that. Using plain http works fine.
-    base = 'http://celestrak.org/NORAD/elements/gp.php'
-    url = base + f'?GROUP={group_name}&FORMAT=json'
-    #url = 'https://www.space-track.org/basicspacedata/query/class/gp/NORAD_CAT_ID/1641,01361,05398,09931,13154,16496,16792,16882,20262,20663,20666,20705,23581,24748,25050,25544,25724,30794,41182,41591,43611,41793,40940,43432,36744,42691,41944,41034,29495,42695,37933/orderby/TLE_LINE1%20ASC/format/3le'
-
-    if not load.exists(filename) or load.days_old(filename) >= max_days:
-        print("File doesn't exist or is too old. Downloading")
-        load.download(url, filename=filename)
-
-    with load.open(filename) as f:
-        raw_json = json.load(f)
-
-    return raw_json
-"""
+parser.add_argument('--group', type=str, default="amateur", help = 'Name of satellite group to use (e.g. radar, StarLink, etc.)')
 
 if __name__ == "__main__":
-    #load_from_file_or_url(group_name='amateur', max_days=7.0)
-    #exit()
-
     # Load configuration file and apply command-line overrides
     try:
         args = parser.load_args_and_overrides(CONFIG_FILE)
@@ -86,13 +62,10 @@ if __name__ == "__main__":
 
     # Read the TLE file
     myloader = Loader('./skyfield-data')
-    #group='Starlink'
-    #group='amateur'
-    group='radar'
     frmt='tle'
     base = 'http://celestrak.org/NORAD/elements/gp.php'
-    url = base + f'?GROUP={group}&FORMAT={frmt}'
-    with myloader.open(url, filename=f'{group}.{frmt}') as f:
+    url = base + f'?GROUP={args.group}&FORMAT={frmt}'
+    with myloader.open(url, filename=f'{args.group}.{frmt}') as f:
         sat_list = list(parse_tle_file(f, ts))
     print(f'Loaded {len(sat_list)} satellites', flush=True)
 
@@ -173,36 +146,30 @@ if __name__ == "__main__":
         if pass_num > max_passes:
             break
 
-    while True:
-        # Let the user select a pass to track
-        if len(all_passes) == 0:
-            print("No passes found")
-            exit(0)
+    # Let the user select a pass to track
+    if len(all_passes) == 0:
+        print("No passes found")
+        exit(0)
 
-        pass_num = 0
-        while not(0 < pass_num <= max_passes):
-            pass_num = int(input("Choose a pass to watch: ")) 
-        pass_num -= 1 # put it back to a zero index
+    pass_num = 0
+    while not(0 < pass_num <= max_passes):
+        pass_num = int(input("Choose a pass to watch: ")) 
+    pass_num -= 1 # put it back to a zero index
 
-        sat_pass = all_passes[pass_num]
-        print(sat_pass)
-        sat = sat_pass.sat
+    sat_pass = all_passes[pass_num]
+    print(sat_pass)
+    sat = sat_pass.sat
 
-        # Print the look plan
-        difference = sat_pass.sat - obs_pos
-        t0 = sat_pass.ascend_time
-        t1 = sat_pass.descend_time
-        look_time = t0
-        time_step = 1 / (24 * 60)   # 1 minute
-        while look_time.utc_datetime() <= t1.utc_datetime():
-            topocentric = difference.at(look_time)
-            dt_str = look_time.utc_datetime().astimezone(TZ)
-            alt, az, distance = topocentric.altaz()
-            print(f'{dt_str} Az = {az.degrees:6.2f} Elev = {alt.degrees:6.2f} Distance = {distance.km:6.2f}')
-            look_time += time_step
+    # Print the look plan
+    difference = sat_pass.sat - obs_pos
+    t0 = sat_pass.ascend_time
+    t1 = sat_pass.descend_time
+    look_time = t0
+    time_step = 1 / (24 * 60)   # 1 minute
+    while look_time.utc_datetime() <= t1.utc_datetime():
+        topocentric = difference.at(look_time)
+        dt_str = look_time.utc_datetime().astimezone(TZ)
+        alt, az, distance = topocentric.altaz()
+        print(f'{dt_str} Az = {az.degrees:6.2f} Elev = {alt.degrees:6.2f} Distance = {distance.km:6.2f}')
+        look_time += time_step
 
-        if input('\nLook at another? (y/n) ' ) == 'y':
-            print()
-            continue
-        else:
-            break
